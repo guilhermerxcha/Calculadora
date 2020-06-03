6fcfcd5bea10790c4cf41ee32f25e7a95d2ed0c8
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-03 14:56:00 -0300

    Criando arquivos base para calculadora

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from  'react'
+import './Calculator.css'
+
+export  default  class  Calculator extends Component{
+    render() {
+        return(
+            // eslint-disable-next-line react/react-in-jsx-scope
+            <div className="calculator">
+
+
+            </div>
+        )
+    }
+}
